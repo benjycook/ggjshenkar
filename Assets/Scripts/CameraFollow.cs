6fc0e52fc82482bb46de8f7dcec9f3e234ebcad5
Benjy Cook <benjytcook@gmail.com>
2018-01-25 13:18:44 +0200

    updated camera script to respect inital horizontal offset

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
+	private float horizontalOffset;
+
+	void Start() {
+		horizontalOffset = player.transform.position.x - transform.position.x;
+
+	}
 
     private void Update()
     {
-        Vector3 target = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+		Vector3 target = new Vector3(player.transform.position.x-horizontalOffset, player.transform.position.y, -10);
         transform.position = Vector3.MoveTowards(transform.position, target, 10);
     }
 }
