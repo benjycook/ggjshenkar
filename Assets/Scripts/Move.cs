c7abc3e19d92e3199f8cb8d05a5b975072b6a957
ROBOT55 <robot55+gitConfig@gmail.com>
2018-01-25 17:30:03 +0200

    bugfixes galore

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -9,6 +9,7 @@ public class Move : MonoBehaviour {
     public bool isMove = true;
     public bool isGrounded = true;
     public float speed = 3;
+	public float accel = 100.0f;
     public float gravityScale = 10f;
     public float forceJump = 5f;
 
@@ -17,6 +18,8 @@ public class Move : MonoBehaviour {
     Vector3 movement = Vector3.zero;
     Vector3 jump = Vector3.up;
 
+	bool inFreeze = false;
+
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
@@ -24,46 +27,33 @@ public class Move : MonoBehaviour {
 
     private void Update()
     {
-        movement = Vector3.right * speed ;
-        
-        rigid.velocity = new Vector3(movement.x, rigid.velocity.y, rigid.velocity.z);
-        
-       
 
-        if (Input.GetKey(KeyCode.Alpha0) || Input.GetKey(KeyCode.Joystick2Button3))
-        {
-            rigid.velocity = new Vector3(0, rigid.velocity.y, rigid.velocity.z);
-        }
+		if (Input.GetKey (KeyCode.Alpha0) || Input.GetKey (KeyCode.Joystick2Button3)) {
+			rigid.velocity = new Vector3 (0, rigid.velocity.y, rigid.velocity.z);
+			inFreeze = true;
+		} else {
+			inFreeze = false;
+		}
 
         if ((Input.GetKey(KeyCode.Space)|| Input.GetKey(KeyCode.Joystick1Button1)) && isGrounded && rigid.velocity.y <= 0)
         {
             rigid.velocity += new Vector3(0, 1, 0) * forceJump;
         }
 
-	
+		isGrounded = Physics.Raycast (transform.position, Vector3.down, 1f);
 
 
     }
 
     private void FixedUpdate()
     {
+		if (!inFreeze && rigid.velocity.x < speed) {
+			rigid.AddForce (Vector3.right * accel, ForceMode.VelocityChange);
+		}
+
         Vector3 gravity = Vector3.down * gravityScale;
         rigid.AddForce(gravity, ForceMode.Acceleration);
-    }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Platform"))
-        {
-            isGrounded = false;
-        }
-    }
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Platform"))
-        {
-            isGrounded = true;
-        }
     }
 
     
