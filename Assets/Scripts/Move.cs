130101edfe73d5c429b492fd6ddb608ee36cfd9e
randmeb <randmeb@gmail.com>
2018-01-25 13:29:34 +0200

    Change Jump and add buttons of joystick

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -10,6 +10,7 @@ public class Move : MonoBehaviour {
     public float forceJump = 5f;
     public bool isGrounded = true;
     public float speed = 3;
+    public float gravityScale = 10f;
 
 
     Vector3 movement = Vector3.zero;
@@ -25,24 +26,29 @@ public class Move : MonoBehaviour {
         movement = Vector3.right * speed ;
         rigid.velocity = new Vector3(movement.x, rigid.velocity.y, rigid.velocity.z);
 
-        if (Input.GetKey(KeyCode.Alpha0))
+        if (Input.GetKey(KeyCode.Alpha0) || Input.GetKey(KeyCode.Joystick1Button3))
         {
             rigid.velocity = new Vector3(0, rigid.velocity.y, rigid.velocity.z);
         }
 
-        //if (Input.GetKey(KeyCode.Space)&&isGrounded)
-        //{
-        //    rigid.velocity = new Vector3(movement.x, jump.y * forceJump, rigid.velocity.z);
-        //}
-        if (Input.GetKey(KeyCode.Space) && isGrounded && rigid.velocity.y <= 0)
+        if ((Input.GetKey(KeyCode.Space)|| Input.GetKey(KeyCode.Joystick1Button1)) && isGrounded && rigid.velocity.y <= 0)
         {
             rigid.velocity += new Vector3(0, 1, 0) * forceJump;
         }
 
-        if (rigid.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        if (rigid.velocity.y > 0 && !(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick1Button1)))
         {
             rigid.velocity += new Vector3(0, -5, 0);
         }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 gravity = Vector3.down * gravityScale;
+        rigid.AddForce(gravity, ForceMode.Acceleration);
     }
 
     private void OnCollisionExit(Collision collision)
